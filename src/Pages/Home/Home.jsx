109750b3354b6ef50fe6cb1b0e501de77d60cd5d
ziad essa <helmyzez@gmail.com>
2024-10-28 14:23:40 +0300

    Navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-dark vh-100 w-100'>
+      <h2>home</h2>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
