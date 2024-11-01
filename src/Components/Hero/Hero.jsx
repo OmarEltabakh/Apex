a52966164a36f77fe2f010f85c46f0eebc866148
ziad essa <helmyzez@gmail.com>
2024-11-01 11:20:54 +0200

    updateNavbar

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
         <h2 className='absolute top-[20%] left-[15%] sizeBackText font md:!text-[130px] sm:!text-[100px] xs:!text-[70px] md:left-[10%] md:top-[30%] xs:top-[35%]'>EVENT 24</h2>
         <div className="container absolute top-[40%] left-[10%]">
             <div className="row">
-                <div className="col-md-7 ">
+                <div className="col-md-7">
                     <h1 className='text-light !text-8xl md:!text-8xl sm:!text-[55px] xs:!text-7xl fw-bold mb-4 font'>Welcome to Apex</h1>
                     <p className='text-light/60 text-[17px] xs:text-[15px] w-75'>
                         Lorem ipsum dolor sit amet consectetur.
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -45,30 +45,30 @@ const Navbar = () => {
   }
 
   return <>
-  <header className= {`${style.header} relative z-10 lg:px-16 md:px-12 sm:px-8  w-full px-32  font-medium flex items-center justify-between fixed-top  ${scrolled ? "bg-black navshadow transition-all" : ""}`} >
+  <header className= {`${style.header} relative z-10 lg:px-16 md:px-12 sm:px-8  w-full px-32  font-medium flex items-center justify-between fixed-top  ${scrolled ? "bg-black navshadow " : ""}`} >
 
   <button className='hidden lg:flex  flex-col justify-center items-center' onClick={handelClick}>
-    <span className={`bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-0.5' : "-translate-y-0.5"}`}></span>
-    <span className={`bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? "opacity-0" : "opacity-100"}`}></span>
-    <span className={`bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1.5' : "translate-y-0.5"}`}></span>
+    <span className={`bg-light block transition-all duration-300 ease-out h-1 w-9 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-0.5' : "-translate-y-0.5"}`}></span>
+    <span className={`bg-light block transition-all duration-300 ease-out h-0.5 w-9 rounded-sm my-1 ${isOpen ? "opacity-0" : "opacity-100"}`}></span>
+    <span className={`bg-light block transition-all duration-300 ease-out h-1 w-9 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1.5' : "translate-y-0.5"}`}></span>
   </button>
 
   
   <div 
   className='container flex justify-between items-center lg:hidden'> 
 
-  <Link to='/' className='w-[110px] md:w-[120px]   top-1  '>
+  <Link to='/' className='w-[90px]  top-1 '>
       <img className='w-100' src={logo} alt="icpc hti logo" />
   </Link>
 
   {/* links */}
   <nav className='flex justify-center align-center'>
-    <CustomLink to='/'  title="Home"  className='mr-3 text-[20px] fw-bold fw-bold text-decoration-none text-light'/>
-    <CustomLink to="/about" title="About Us" className='mx-3 text-[20px] fw-bold text-decoration-none text-light'/>
-    <CustomLink to="/Team" title="Team subdivision" className='mx-3 text-[20px] fw-bold text-decoration-none text-light'/>
-    <CustomLink to="/Competitions" title="Competitions" className='mx-3 text-[20px] fw-bold text-decoration-none text-light'/>
-    <CustomLink to="/Projects" title="Projects" className='mx-3 text-[20px] fw-bold text-decoration-none text-light'/>
-    <CustomLink to="/Join" title="Join us" className='mx-3 text-[20px] fw-bold text-decoration-none text-light'/>
+    <CustomLink to='/'  title="Home"  className='mr-3 text-[18px] fw-bold fw-bold text-decoration-none text-light'/>
+    <CustomLink to="/about" title="About Us" className='mx-3 text-[18px] fw-bold text-decoration-none text-light'/>
+    <CustomLink to="/Team" title="Team subdivision" className='mx-3 text-[18px] fw-bold text-decoration-none text-light'/>
+    <CustomLink to="/Competitions" title="Competitions" className='mx-3 text-[18px] fw-bold text-decoration-none text-light'/>
+    <CustomLink to="/Projects" title="Projects" className='mx-3 text-[18px] fw-bold text-decoration-none text-light'/>
+    <CustomLink to="/Join" title="Join us" className='mx-3 text-[18px] fw-bold text-decoration-none text-light'/>
     
   </nav>
 
@@ -82,19 +82,19 @@ animate={{scale:1 , opacity:1 }}
 transition={{duration:1}}
 className='z-30 login rounded-lg backdrop-blur-md bg-dark/50 py-[70px] min-w-[70vw] flex flex-col  justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
   <nav className='flex items-center flex-col justify-center w-100'>
-    <CustomLink to='/'  title="Home"  className='mx-3 text-[20px] fw-bold text-decoration-none text-light' toggle={handelClick}/>
-    <CustomLink to="/about" title="About Us" className='mx-3 text-[20px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
-    <CustomLink to="/Team" title="Team subdivision" className='mx-3 text-[20px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
-    <CustomLink to="/Competitions" title="Competitions" className='mx-3 text-[20px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
-    <CustomLink to="/Projects" title="Projects" className='mx-3 text-[20px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
-    <CustomLink to="/Join" title="Join us" className='mx-3 text-[20px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
+    <CustomLink to='/'  title="Home"  className='mx-3 text-[18px] fw-bold text-decoration-none text-light' toggle={handelClick}/>
+    <CustomLink to="/about" title="About Us" className='mx-3 text-[18px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
+    <CustomLink to="/Team" title="Team subdivision" className='mx-3 text-[18px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
+    <CustomLink to="/Competitions" title="Competitions" className='mx-3 text-[18px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
+    <CustomLink to="/Projects" title="Projects" className='mx-3 text-[18px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
+    <CustomLink to="/Join" title="Join us" className='mx-3 text-[18px] fw-bold mt-2 text-decoration-none text-light' toggle={handelClick}/>
 
 
   </nav>
 </div>
   : null
 }
-        <img className='hidden lg:flex w-25' src={logo} alt="" />
+        <img className=' hidden lg:flex w-[70px]' src={logo} alt="" />
 
   </header>
   </>
